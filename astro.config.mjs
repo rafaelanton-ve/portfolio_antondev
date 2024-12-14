f12a50b96c8093214e800362da269aeea2109454
ar.anton.1960 <ar.anton.1960@gmail.com>
2024-12-14 04:40:26 +0000

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://simple-portfolio.vicbox.dev/',
+    site: 'https://antondev.github.io/portfolio_antondev',
     integrations: [mdx(), react(), tailwind({
         applyBaseStyles: false,
     }), icon(), sitemap()],
